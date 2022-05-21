e205213d11ad5643eb4f57e82b890fb88e49eba4
Alexander Rovang <DXRovang@gmail.com>
2022-05-21 10:15:57 -0400

    first commit

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,20 @@
+class FriendsController < ApplicationController
+  def index
+    @friend = Friend.all.sample
+  end
+
+  def edit
+    @friend = Friend.all.sample
+  end
+  def update
+    @friend = Friend.find_by(id: params[:id])
+    @@friendName = @friend.name
+    binding.pry
+    # Friend.all.delete_at(@friend.id)
+    redirect_to friends_path
+  end
+
+  def friend_params
+    params.require(:friend).permit(:name, :id)
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+friends = Friend.create([
+  { name: 'dan' }, { name: 'brian' }, { name: 'sean' }, { name: 'lane' }, 
+  { name: 'james' }, { name: 'will' }, { name: 'emilyG' }, { name: 'amy' }, 
+  { name: 'vonD' }, { name: 'angie' }, { name: 'jm' }, { name: 'martin' }, 
+  { name: 'minda' }, { name: 'britP' }, { name: 'sabrina' }, { name: 'xavier' }, 
+  { name: 'prieto' }, { name: 'rosengarten' }, { name: 'kevinH' }, { name: 'hicks' }, 
+  { name: 'je' }, { name: 'teddy' }, { name: 'clowe' }, { name: 'courtney' }, 
+  { name: 'fecteau' }, { name: 'mandra' }, { name: 'jainardo' }, { name: 'bone' }, 
+  { name: 'kale' }, { name: 'farns' }, { name: 'fifer' }, { name: 'ford' }, 
+  { name: 'jasper' }, { name: 'luke' }, { name: 'joeBeam' }, { name: 'aaronJ' }, 
+  { name: 'madore' }, { name: 'sharf' }, { name: 'fred' }, { name: 'masse' }, 
+  { name: 'eisenstein' }, { name: 'ian' }, { name: 'susanO' }, { name: 'paulR' }, 
+  { name: 'brandon' }, { name: 'winden' }, { name: 'ada' }, { name: 'scottW' }, 
+  { name: 'daveV' }, { name: 'monteith' }, { name: 'jessica' } 
+  
+  
+  ])
